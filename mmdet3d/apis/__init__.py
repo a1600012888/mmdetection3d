1188d74459fc8b8a3d7016d21f501c9e6a077573
a1600012888 <1600012888@pku.edu.cn>
2021-09-28 02:59:20 +0800

    fix import error

--- mmdet3d/apis/__init__.py
+++ mmdet3d/apis/__init__.py
@@ -3,7 +3,7 @@ from .inference import (convert_SyncBN, inference_detector,
                         show_result_meshlab)
 from .test import single_gpu_test
 from .train import train_tracker
-from .visuailize_normal import single_gpu_visualize, multi_gpu_visualize
+from .visualize_normal import single_gpu_visualize, multi_gpu_visualize
 
 __all__ = [
     'inference_detector', 'init_detector', 'single_gpu_test',
